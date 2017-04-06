126dac3e83b44907dbe6cd6317d7d9578bc10247
Steve Eichten <sre801@raijin4.(none)>
2017-04-06 11:31:19 +1000

    new tile tree merge method

--- methylation_tile_tree.R
+++ methylation_tile_tree.R
@@ -12,40 +12,42 @@ current=Sys.time()
 
 #install.packages('data.table')
 #library(data.table)
-install.packages('dendextend')
+#install.packages('dendextend')
 library(dendextend)
 #functions
 ##############
 merge_wigs <- function(filelist){
-  all=read.delim(filelist[1],head=F,skip=1)
-  all=all[,1:4]
-  colnames(all)[4]=filelist[1]
-  for(i in 2:length(filelist)){
-    temp=read.delim(filelist[i],head=F,skip=1)
-    temp=temp[,1:4]
-    colnames(temp)[4]=filelist[i]
-    all=merge(all,temp,by=c('V1','V2','V3'),all=T)
+  all=list()
+  for (i in 1:length(filelist)){
+    tmp = read.delim(filelist[i],head=F,skip=1)
+    tmp=tmp[,1:4]
+    colnames(tmp)[4]=filelist[i]
+    filename=files[i]
+    all[[filename]]=tmp
   }
-  return(all)
+  goob = all %>% Reduce(function(dtf1,dtf2) full_join(dtf1,dtf2,by=c('V1','V2','V3')), .)
+  return(goob)
 }
 
 calculate_dist <- function(data,column){
   meta=read.delim(metapath,head=F)
   current=names(data[,4:ncol(data)])
-  c2=matrix(unlist(strsplit(current,'_')),ncol=7,byrow=T)
+  c2=matrix(unlist(strsplit(current,'_')),ncol=3,byrow=T)
   c2=c2[,1]
-  c2=gsub('^..','',c2)
+  #split by period for the SRA downloaded files
+  c2=matrix(unlist(strsplit(c2,'\\.')),ncol=3,byrow=T)
+  c2=c2[,1]
   meta82=meta[meta$V1 %in% c2,]
   meta82=meta82[match(c2,meta82$V1),]
   colnames(data)[4:ncol(data)]=as.character(meta82[,column])
-  hc=hclust(as.dist(1-cor(data[,4:ncol(data)],use='pairwise.complete.obs')))
+  hc=hclust(as.dist(1-cor(as.matrix(data[,4:ncol(data)]),use='pairwise.complete.obs')))
   return(hc)
 }
 
-plot_dendro <- function(hc,title){
-  pdf(paste(title,'methylation_dendrogram.',current,'.pdf',sep='_'),height=5,width=30)
-  dendro_labels=gsub('_.*','',gsub('\\.[0-9]*','',hc$labels))
-  dendro_colors=as.numeric(as.factor(dendro_labels))
+plot_dendro <- function(hc,column,title){
+  pdf(paste(title,'methylation_dendrogram.',current,'.pdf',sep='_'),height=9,width=60)
+  dendro_labels=gsub('_.*','',gsub('\\.[0-9]*','',as.character(meta82[,column])))
+  dendro_colors=as.numeric(as.factor(gsub('_.*','',gsub('\\.[0-9]*','',as.character(meta82[,8])))))
   hc=as.dendrogram(hc)
   #labels(hc)=dendro_labels[order.dendrogram(hc)]
   labels_colors(hc) = dendro_colors[order.dendrogram(hc)]
@@ -83,11 +85,15 @@ write.table(all,paste(context,'_alltiles_merged.',current,'.txt',sep=''),sep='\t
 
 
 tile.dist=calculate_dist(all,6)
-plot_dendro(tile.dist,paste(context,'_sample',sep=''))
+saveRDS(tile.dist,paste(context,'_tile.dist.rds',sep=''))
+plot_dendro(tile.dist,6,paste(context,'_sample',sep=''))
 tile.cor=calculate_cor(all,6)
+saveRDS(tile.cor,paste(context,'_tile.cor.rds',sep=''))
 plot_heatmap(tile.cor,paste(context,'_sample',sep=''))
 
 tile.dist=calculate_dist(all,8)
-plot_dendro(tile.dist,paste(context,'_clonal',sep=''))
+saveRDS(tile.dist,paste(context,'_tile.dist.sample.rds',sep=''))
+plot_dendro(tile.dist,6,paste(context,'_clonal',sep=''))
 tile.cor=calculate_cor(all,8)
+saveRDS(tile.cor,paste(context,'_tile.cor.sample.rds',sep=''))
 plot_heatmap(tile.cor,paste(context,'_sample',sep=''))
